06d2ca7176e8f5a2274c8e1e1c6965759e9ca5ae
haraldhultin <haraldhultin@gmail.com>
2021-10-15 03:35:41 +0200

    test2 commit to branch "test"

--- magicFactory/Program.cs
+++ magicFactory/Program.cs
@@ -7,6 +7,7 @@ namespace magicFactory
     {
         static void Main(string[] args)
         {
+            // test2 commit branch "test" <-------
             // testa commit.. <------
             Game newGame = new Game();    // onädigt? lägga i game? göra Start() static?        
             newGame.Start();         
